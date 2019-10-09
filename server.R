42d536d01c4fa59fbcae04c99c4106d019924763
José Jaime Méndez Flores <you@example.com>
2019-10-09 16:43:34 +0200

    Corrections on dates

--- server.R
+++ server.R
@@ -11,14 +11,12 @@ server <- function(input, output) {
     format.Date(input$daterange[2] , c("%Y-%m-%dT%H:%mZ"))
   })
   
-  observe({print(fromDate())})
-  
   output$energyplot <- renderPlot({
-    Team05Lab05::pie_API_2(input$selected_region, fromDate, toDate)
+    Team05Lab05::pie_API_2(input$selected_region, fromDate(), toDate())
   })
   
   output$results <- renderTable({
-    Team05Lab05::check_fuel_region_time(input$selected_region, "2018-11-16T09:00Z", "2018-12-16T09:00Z")
+    Team05Lab05::check_fuel_region_time(input$selected_region, fromDate(), toDate())
   }, align = "c")
     
   }
